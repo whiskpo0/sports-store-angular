6decaa9a9c9a936e6854a0b5c73d432dc95deb57
Adam M <you@example.com>
2020-02-09 21:03:14 -0600

    Created and added the store directive. Displays categories and products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.model'; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/store.model.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser"; 
+import { FormsModule } from "@angular/forms";
+import { ModelModule } from "../model/model.model";
+import { StoreComponent } from "./store.component";
+import { CounterDirective } from "./counter.directive"; 
+
+@NgModule({ 
+    imports: [ModelModule, BrowserModule, FormsModule], 
+    declarations: [StoreComponent, CounterDirective], 
+    exports: [StoreComponent]
+})
+
+export class StoreModule { }
