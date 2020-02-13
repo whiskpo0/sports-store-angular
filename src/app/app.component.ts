22cf3a04f6305e4530b5702d7179df0c32486a7f
Adam M <you@example.com>
2020-02-13 10:05:18 -0600

    Finished the store front and check out using RESTful web services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: "app",
  
-  template: "<store></store>",
+  template: "<router-outlet></router-outlet>",
 })
 export class AppComponent {
   title = 'sports-store';
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core"; 
 import { Product } from "../model/product.model"; 
 import { ProductRepository } from "../model/product.repository"; 
+import { Cart } from "../model/cart.model"; 
+import { Router } from "@angular/router"; 
 
 @Component({ 
     selector: "store", 
@@ -12,7 +14,9 @@ export class StoreComponent {
     public productsPerPage = 4; 
     public selectedPage = 1; 
 
-    constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository,
+         private cart: Cart, 
+         private router: Router) { }
 
     get products(): Product[] { 
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage
@@ -42,6 +46,10 @@ export class StoreComponent {
             .getProducts(this.selectedCategory).length / this.productsPerPage)
     }
 
+    addProductToCart(product: Product) { 
+        this.cart.addLine(product); 
+        this.router.navigateByUrl("/cart"); 
+    }
     // get pageNumbers(): number[] { 
     //     return Array(Math.ceil(this.repository
     //         .getProducts(this.selectedCategory).length / this.productsPerPage)) 
